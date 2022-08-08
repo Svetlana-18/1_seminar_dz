1537e5d66d749e77635efd1f42b95b12e0f4395a
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-08-09 00:36:15 +0300

    перенесла решения задач из черновика

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -4,3 +4,19 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Введите целое число 1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.WriteLine($"Первое число {num1} равно второму числу {num2}");
+}
+else if (num1 > num2)
+{
+    Console.WriteLine($"Первое число {num1} больше второго числа {num2}");
+}
+else
+{
+    Console.WriteLine($" Первое число {num1} меньше второго числа {num2}");
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,18 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите целое число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine($"Неверный ввод,введите положительное число");
+}
+else if (num % 2 == 0)
+{
+    Console.WriteLine($" Число {num} четное");
+}
+else
+{
+    Console.WriteLine($" Число {num} нечетное");
+}
